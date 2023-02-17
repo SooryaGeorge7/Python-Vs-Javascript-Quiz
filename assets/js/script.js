3fa981f29296b17df704bacf3e8d9c923bdb41a7
SooryaGeorge7 <sooryageorge07@gmail.com>
2023-02-17 23:39:00 +0000

    create variables using getElementById

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+let playButton = document.getElementById('start-game');
+let userName = document.getElementById('username');
+let homePage = document.getElementById('home-page');
+let gamePage = document.getElementById('game-page');
+
+let gameoverPage = document.getElementById('gameover-area');
+let questionArea = document.getElementById('questions');
+let python = document.getElementById('python');
+let javaScript = document.getElementById('javascript');
