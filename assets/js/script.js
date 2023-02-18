e3dbf2794a59e48597aee61e93bdc17d3cfbb4ed
SooryaGeorge7 <sooryageorge07@gmail.com>
2023-02-18 00:02:26 +0000

    added if,else if, else statments to validate username

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ let playButton = document.getElementById('start-game');
 let userName = document.getElementById('username');
 let homePage = document.getElementById('home-page');
 let gamePage = document.getElementById('game-page');
-
+let validationMessage = document.getElementById('validation');
 let gameoverPage = document.getElementById('gameover-area');
 let questionArea = document.getElementById('questions');
 let python = document.getElementById('python');
@@ -12,6 +12,14 @@ let javaScript = document.getElementById('javascript');
 let usernameLength = "*Please enter a name longer than 2 charectors*";
 let checkSpace = "Please enter a name with no spaces";
 function validateMessage() {
-    
+    if(userName.value.length <= parseInt(2)) {
+        validationMessage.innerHTML = usernameLength;
+    }
+    else if (userName.value.indexOf(" ") >= 0) {
+        validationMessage.innerHTML = checkSpace;
+    }
+    else {
+        playGame();
+    }
 
 }
